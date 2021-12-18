763ff131a4944f3e3b1386f96cee2ebc0ea2a736
sakhawat9 <sakhawathossain7969@gmail.com>
2021-12-18 22:28:44 +0600

    [ADD] css

--- src/components/containers/ImageGallery.js
+++ src/components/containers/ImageGallery.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { GrView } from "react-icons/gr";
+import { BsFillEyeFill } from "react-icons/bs";
 import image1 from "../../assets/images/home-2-gallery-1.jpg";
 import image2 from "../../assets/images/home-2-gallery-2.jpg";
 import image3 from "../../assets/images/home-2-gallery-3.jpg";
@@ -10,7 +10,7 @@ import image6 from "../../assets/images/home-2-gallery-6.jpg";
 const ImageGallery = () => {
   return (
     <>
-      <div className="image-gallery-area-v2 section-padding-t section-padding-secondary-b">
+      <div className="section-padding-t section-padding-secondary-b">
         <div className="container mx-auto px-8">
           <div className="section-header-two text-center">
             <div className="featured-products__header">
@@ -23,67 +23,39 @@ const ImageGallery = () => {
             <div className="md:col-span-8 col-span-12">
               <div className="grid gap-4 grid-cols-12">
                 <div className="lg:col-span-12">
-                  <div className="single-gallery border-left">
-                    <img
-                      className="gallery-image"
-                      src={image1}
-                      alt="gallery"
-                    />
-                    <div className="popuo-overlay">
-                      <a
-                        className="popup-image"
-                        href={image1}
-                      >
-                        <GrView />
+                  <div className="single-gallery single-gallery__left">
+                    <img src={image1} alt="gallery" />
+                    <div className="popup-overlay">
+                      <a href={image1}>
+                        <BsFillEyeFill />
                       </a>
                     </div>
                   </div>
                 </div>
                 <div className="md:col-span-6">
-                  <div className="single-gallery border-bottom">
-                    <img
-                      className="gallery-image"
-                      src={image2}
-                      alt="gallery"
-                    />
-                    <div className="popuo-overlay">
-                      <a
-                        className="popup-image"
-                        href={image2}
-                      >
-                        <GrView />
+                  <div className="single-gallery single-gallery__bottom">
+                    <img src={image2} alt="gallery" />
+                    <div className="popup-overlay">
+                      <a href={image2}>
+                        <BsFillEyeFill />
                       </a>
                     </div>
                   </div>
                 </div>
                 <div className="md:col-span-6">
                   <div className="single-gallery">
-                    <img
-                      className="gallery-image"
-                      src={image3}
-                      alt="gallery"
-                    />
-                    <div className="popuo-overlay">
-                      <a
-                        className="popup-image"
-                        href={image3}
-                      >
-                        <GrView />
+                    <img src={image3} alt="gallery" />
+                    <div className="popup-overlay">
+                      <a href={image3}>
+                        <BsFillEyeFill />
                       </a>
                     </div>
                   </div>
                   <div className="single-gallery">
-                    <img
-                      className="gallery-image"
-                      src={image4}
-                      alt="gallery"
-                    />
-                    <div className="popuo-overlay">
-                      <a
-                        className="popup-image"
-                        href={image4}
-                      >
-                        <GrView />
+                    <img src={image4} alt="gallery" />
+                    <div className="popup-overlay">
+                      <a href={image4}>
+                        <BsFillEyeFill />
                       </a>
                     </div>
                   </div>
@@ -91,33 +63,19 @@ const ImageGallery = () => {
               </div>
             </div>
             <div className="md:col-span-4 col-span-12">
-              <div className="single-gallery border-top">
-                <img
-                  className="gallery-image"
-                  src={image5}
-                  alt="gallery"
-                />
-                <div className="popuo-overlay">
-                  <a
-                    className="popup-image"
-                    href={image5}
-                  >
-                    <GrView />
+              <div className="single-gallery single-gallery__top">
+                <img src={image5} alt="gallery" />
+                <div className="popup-overlay">
+                  <a href={image5}>
+                    <BsFillEyeFill />
                   </a>
                 </div>
               </div>
               <div className="single-gallery">
-                <img
-                  className="gallery-image"
-                  src={image6}
-                  alt="gallery"
-                />
-                <div className="popuo-overlay">
-                  <a
-                    className="popup-image"
-                    href={image6}
-                  >
-                    <GrView />
+                <img src={image6} alt="gallery" />
+                <div className="popup-overlay">
+                  <a href={image6}>
+                    <BsFillEyeFill />
                   </a>
                 </div>
               </div>
