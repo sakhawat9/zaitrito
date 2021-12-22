3994cb563f82b116243b065160d43a4b756d908b
sakhawat9 <sakhawathossain7969@gmail.com>
2021-12-22 10:24:11 +0600

    [ADD] castom login page

--- src/components/containers/SignInPage.js
+++ src/components/containers/SignInPage.js
@@ -1,88 +1,95 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable react/style-prop-object */
 import React from "react";
+import { useForm } from "react-hook-form";
 import { FiUser } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 const SignInPage = () => {
-  return (
-    <div className="sign-in-page section-padding text-center">
-      <div className="container mx-auto px-8">
-        <div className="grid grid-cols-12 items-center justify-center">
-          <div className="col-span-12 lg:col-span-5">
-            <div className="login-wrap">
-              <div className="icon flex items-center justify-center">
-                <span className="far fa-user"><FiUser /></span>
-              </div>
-              <h1 className="text-center mb-4">Sign In</h1>
-              <form className="login-form">
-                <div className="form-group">
-                  <input
-                    type="text"
-                    className="form-control rounded-left"
-                    placeholder="Username"
-                    required=""
-                    autocomplete="off"
-                    style={{
-                      backgroundImage:
-                        "url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAAAXNSR0IArs4c6QAAAPhJREFUOBHlU70KgzAQPlMhEvoQTg6OPoOjT+JWOnRqkUKHgqWP4OQbOPokTk6OTkVULNSLVc62oJmbIdzd95NcuGjX2/3YVI/Ts+t0WLE2ut5xsQ0O+90F6UxFjAI8qNcEGONia08e6MNONYwCS7EQAizLmtGUDEzTBNd1fxsYhjEBnHPQNG3KKTYV34F8ec/zwHEciOMYyrIE3/ehKAqIoggo9inGXKmFXwbyBkmSQJqmUNe15IRhCG3byphitm1/eUzDM4qR0TTNjEixGdAnSi3keS5vSk2UDKqqgizLqB4YzvassiKhGtZ/jDMtLOnHz7TE+yf8BaDZXA509yeBAAAAAElFTkSuQmCC&quot;)",
-                      backgroundRepeat: "no-repeat",
-                      backgroundAttachment: "scroll",
-                      backgroundSize: "16px 18px",
-                      backgroundPosition: "98% 50%",
-                    }}
-                  />
-                </div>
-                <div className="form-group flex">
-                  <input
-                    type="password"
-                    className="form-control rounded-left"
-                    placeholder="Password"
-                    required=""
-                    autocomplete="off"
-                    style={{
-                      backgroundImage:
-                        "url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAAAXNSR0IArs4c6QAAAPhJREFUOBHlU70KgzAQPlMhEvoQTg6OPoOjT+JWOnRqkUKHgqWP4OQbOPokTk6OTkVULNSLVc62oJmbIdzd95NcuGjX2/3YVI/Ts+t0WLE2ut5xsQ0O+90F6UxFjAI8qNcEGONia08e6MNONYwCS7EQAizLmtGUDEzTBNd1fxsYhjEBnHPQNG3KKTYV34F8ec/zwHEciOMYyrIE3/ehKAqIoggo9inGXKmFXwbyBkmSQJqmUNe15IRhCG3byphitm1/eUzDM4qR0TTNjEixGdAnSi3keS5vSk2UDKqqgizLqB4YzvassiKhGtZ/jDMtLOnHz7TE+yf8BaDZXA509yeBAAAAAElFTkSuQmCC&quot;)",
-                      backgroundRepeat: "no-repeat",
-                      backgroundAttachment: "scroll",
-                      backgroundSize: "16px 18px",
-                      backgroundPosition: "98% 50%",
-                      cursor: "auto",
-                    }}
-                  />
-                </div>
-                <div className="form-group">
-                  <button
-                    type="button"
-                    className="form-control  primary-btn rounded submit px-3 primary-btn"
-                  >
-                    Login
-                  </button>
-                </div>
-                <div className="remember-box form-group md:flex justify-between mb-0">
-                  <div>
-                    <label className="checkbox-wrap">
-                      Remember Me
-                      <input type="checkbox" checked="checked" />
-                      <span className="checkmark"></span>
-                    </label>
-                  </div>
-                  <div className="md:text-end lg:text-end">
-                    <a href="#" className="forget-password-link">
-                      Forgot Password?
-                    </a>
-                  </div>
-                </div>
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
 
-                <div className="already-have-account">
-                  Don't have an account?
-                  <a href="sign-up.html" className="forget-password-link">
-                    Sign Up
-                  </a>
-                </div>
-              </form>
-            </div>
-          </div>
+  const submitHandler = {};
+  return (
+    <div className="login">
+      <div className="login__wrapper">
+        <div className="icon flex items-center justify-center">
+          <span className="far fa-user">
+            <FiUser />
+          </span>
         </div>
+        <h1 className="text-center font-medium text text-5xl">Sign In</h1>
+        <form className="login__form" onSubmit={handleSubmit(submitHandler)}>
+          <input
+            type="email"
+            name="Email"
+            {...register("email", {
+              required: {
+                value: true,
+                message: "You most enter email address",
+              },
+              minLength: {
+                value: 8,
+                message: "This is not long enough to be an email",
+              },
+              maxLength: {
+                value: 120,
+                message: "This is too long",
+              },
+              pattern: {
+                value: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+                message: "invalid email address",
+              },
+            })}
+            style={{ fontSize: "1.5rem" }}
+            className={`
+         ${errors.email ? "ring-2 ring-red-500" : null}`}
+            placeholder="Email"
+          />
+          <span className="py-2 text-sm text-red-400">
+            {errors?.email?.message}
+          </span>
+          <input
+            type="password"
+            name="password"
+            {...register("password", {
+              required: {
+                value: true,
+                message: "You most enter password",
+              },
+              minLength: {
+                value: 6,
+                message: "Password lenth is more then 5",
+              },
+            })}
+            style={{ fontSize: "1.5rem" }}
+            className={`
+             ${errors.password ? "ring-2 ring-red-500" : null}`}
+            placeholder="password"
+          />
+          <span className="py-2 text-sm text-red-400">
+            {errors?.password?.message}
+          </span>
+          <span className="w-full">
+            <input
+              type="submit"
+              className="cursor-pointer text-white bg-red-600 hover:bg-red-700"
+              style={{ transition: "all 0.3s linear" }}
+              value="LogIn"
+            />
+          </span>
+        </form>
+        <p className="text-center block text-2xl">
+          Don't have an account?
+          <Link href="/signUp">
+            <a className="border-b border-b-red-600 font-medium hover:text-red-600">
+              Sign up
+            </a>
+          </Link>
+        </p>
       </div>
     </div>
   );
