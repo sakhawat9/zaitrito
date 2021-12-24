b1a81f9c4dfed8082b8bf366d07eb01222d948c5
sakhawat9 <sakhawathossain7969@gmail.com>
2021-12-24 10:34:36 +0600

    ERROR fixc

--- src/components/containers/AboutUsPage.js
+++ src/components/containers/AboutUsPage.js
@@ -3,31 +3,31 @@ import React from "react";
 
 const AboutUsPage = () => {
   return (
-    <div class="about-us-area section-padding">
-      <div class="container mx-auto px-8">
-        <div class="grid grid-cols-12 lg:items-center">
-          <div class="lg:col-span-5 md:col-span-6 col-span-12">
-            <div class="about-us-image">
+    <div className="about-us-area section-padding">
+      <div className="container mx-auto px-8">
+        <div className="grid grid-cols-12 lg:items-center">
+          <div className="lg:col-span-5 md:col-span-6 col-span-12">
+            <div className="about-us-image">
               <img src="http://zairito-html.zainiktheme.com/demo/assets/images/aboutus-image.png" alt="about us image" />
             </div>
           </div>
           <div className="lg:col-span-1"></div>
-          <div class="lg:col-span-5 col-span-12 md:col-span-6">
-            <div class="about-us-content">
-              <div class="section-header-area">
-                <h3 class="sub-title">About Us</h3>
-                <h2 class="section-title">
+          <div className="lg:col-span-5 col-span-12 md:col-span-6">
+            <div className="about-us-content">
+              <div className="section-header-area">
+                <h3 className="sub-title">About Us</h3>
+                <h2 className="section-title">
                   Innovative solutions <br /> to boost your projects
                 </h2>
               </div>
-              <p class="about-us-text">
+              <p className="about-us-text">
                 Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
                 posuere consectetur sed, convallis at tellus. Donec rutrum
                 congue leo eget malesuada. Curabitur arcu erat, accumsan id
                 imperdiet et, porttitor at sem. Vestibulum ac diam sit amet quam
                 vehicula elementum sed sit amet{" "}
               </p>
-              <p class="about-us-text">
+              <p className="about-us-text">
                 Praesent sapien massa, convallis a pellentesque nec, egestas non
                 nisi. Vivamus suscipit tortor eget felis porttitor volutpat. Sed
                 porttitor lectus nibh. Nulla porttitor accumsan tincidunt.
--- src/components/containers/ServiceArea.js
+++ src/components/containers/ServiceArea.js
@@ -2,77 +2,77 @@ import React from "react";
 
 const ServiceArea = () => {
   return (
-    <div class="service-area section-bg">
-      <div class="container p-0">
-        <div class="grid grid-cols-12 items-center g-0">
-          <div class="lg:col-span-6 col-span-12">
-            <div class="service-left"></div>
+    <div className="service-area">
+      <div>
+        <div className="grid grid-cols-12 items-center g-0">
+          <div className="lg:col-span-6 col-span-12">
+            <div className="service-left"></div>
           </div>
-          <div class="lg:col-span-6 col-span-12">
-            <div class="service-lsit">
-              <div class="grid grid-cols-12">
-                <div class="md:col-span-6 col-span-6">
-                  <div class="single-service">
-                    <div class="service-icon">
+          <div className="lg:col-span-6 col-span-12">
+            <div className="service-lsit">
+              <div className="grid grid-cols-12">
+                <div className="md:col-span-6 col-span-6">
+                  <div className="single-service">
+                    <div className="service-icon">
                       <img
                         src="http://zairito-html.zainiktheme.com/demo/assets/images/service-icon-1.png"
                         alt="service-icon"
                       />
                     </div>
-                    <div class="service-info">
-                      <h3 class="service-title">All Day Comfort</h3>
-                      <p class="service-content">
+                    <div className="service-info">
+                      <h3 className="service-title">All Day Comfort</h3>
+                      <p className="service-content">
                         We believe getting dressed should be the easiest part of
                         your day.
                       </p>
                     </div>
                   </div>
                 </div>
-                <div class="md:col-span-6 col-span-6">
-                  <div class="single-service">
-                    <div class="service-icon">
+                <div className="md:col-span-6 col-span-6">
+                  <div className="single-service">
+                    <div className="service-icon">
                       <img
                         src="http://zairito-html.zainiktheme.com/demo/assets/images/service-icon-2.png"
                         alt="service-icon"
                       />
                     </div>
-                    <div class="service-info">
-                      <h3 class="service-title">All Day Comfort</h3>
-                      <p class="service-content">
+                    <div className="service-info">
+                      <h3 className="service-title">All Day Comfort</h3>
+                      <p className="service-content">
                         We believe getting dressed should be the easiest part of
                         your day.
                       </p>
                     </div>
                   </div>
                 </div>
-                <div class="md:col-span-6 col-span-6">
-                  <div class="single-service">
-                    <div class="service-icon">
+                <div className="md:col-span-6 col-span-6">
+                  <div className="single-service">
+                    <div className="service-icon">
                       <img
                         src="http://zairito-html.zainiktheme.com/demo/assets/images/service-icon-3.png"
                         alt="service-icon"
                       />
                     </div>
-                    <div class="service-info">
-                      <h3 class="service-title">All Day Comfort</h3>
-                      <p class="service-content">
+                    <div className="service-info">
+                      <h3 className="service-title">All Day Comfort</h3>
+                      <p className="service-content">
                         We believe getting dressed should be the easiest part of
                         your day.
                       </p>
                     </div>
                   </div>
                 </div>
-                <div class="md:col-span-6 col-span-6">
-                  <div class="single-service">
-                    <div class="service-icon">
+                <div className="md:col-span-6 col-span-6">
+                  <div className="single-service">
+                    <div className="service-icon">
                       <img
                         src="http://zairito-html.zainiktheme.com/demo/assets/images/service-icon-4.png"
                         alt="service-icon"
                       />
                     </div>
-                    <div class="service-info">
-                      <h3 class="service-title">All Day Comfort</h3>
-                      <p class="service-content">
+                    <div className="service-info">
+                      <h3 className="service-title">All Day Comfort</h3>
+                      <p className="service-content">
                         We believe getting dressed should be the easiest part of
                         your day.
                       </p>
