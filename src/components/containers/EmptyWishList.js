9b3cdc57323f2be6fb140552befe485ebb9b36a5
sakhawat9 <sakhawathossain7969@gmail.com>
2022-01-06 21:53:56 +0600

    [ADD] empty wish list

--- src/components/containers/EmptyWishList.js
+++ src/components/containers/EmptyWishList.js
@@ -3,18 +3,18 @@ import EmptyBox from "../../assets/images/empty-box.png";
 
 const EmptyWishList = () => {
   return (
-    <div class="empty-wish-list section-padding">
-      <div class="container">
-        <div class="empty-box-wrap text-center">
+    <div className="empty-wish-list section-padding">
+      <div className="container">
+        <div className="empty-box-wrap text-center">
           <div className="flex justify-center">
-            <img class="empty-box-img" src={EmptyBox} alt="empty-box" />
+            <img className="empty-box-img" src={EmptyBox} alt="empty-box" />
           </div>
-          <h2 class="empty-box-title">Your Wish List Is Empty</h2>
-          <p class="empty-box-content">
+          <h2 className="empty-box-title">Your Wish List Is Empty</h2>
+          <p className="empty-box-content">
             Nulla porttitor accumsan tincidunt Sed porttitor lectus nibh. Nulla
             quis lorem
           </p>
-          <a href="shop-grid-left-sidebar.html" class="primary-btn">
+          <a href="shop-grid-left-sidebar.html" className="primary-btn">
             Add Now
           </a>
         </div>
